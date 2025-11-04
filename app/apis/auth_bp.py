327a448189962c320c5f81e7716fd7a48235ce1f
Cuongit1-hust <cuongct0902@gmail.com>
2025-11-04 14:49:52 +0700

    update auth blueprint

--- app/apis/auth_bp.py
+++ app/apis/auth_bp.py
@@ -1,9 +1,21 @@
 # app/apis/auth_bp.py
 from sanic import Blueprint
-from app.views.auth_view import RegisterView, LoginView, LogoutView
+from app.views.auth.login_view import LoginView
+from app.views.auth.logout_view import LogoutView
+from app.views.auth.refresh_view import RefreshView
+from app.views.auth.sessions_view import SessionsView
+from app.views.auth.otp_view import OTPView
+from app.views.auth.change_password_view import ChangePasswordView
+from app.views.auth.unlock_view import UnlockView
 
 auth_bp = Blueprint('auth_blueprint', url_prefix='/auth')
 
-auth_bp.add_route(RegisterView.as_view(), '/register')
 auth_bp.add_route(LoginView.as_view(), '/login')
 auth_bp.add_route(LogoutView.as_view(), '/logout')
+auth_bp.add_route(RefreshView.as_view(), '/refresh')
+# auth_bp.add_route(VerifyToken.as_view(), '/verify')
+# auth_bp.add_route(RevokeToken.as_view(), '/token')
+auth_bp.add_route(SessionsView.as_view(), '/sessions')
+auth_bp.add_route(OTPView.as_view(), '/otp')
+auth_bp.add_route(ChangePasswordView.as_view(), '/change-password')
+auth_bp.add_route(UnlockView.as_view(), '/unlock')
