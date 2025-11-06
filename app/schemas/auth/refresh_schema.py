e588fcbbf519dfe377037258e8d631eed4732b72
Cuongit1-hust <cuongct0902@gmail.com>
2025-11-06 18:25:50 +0700

    add user admin schema

--- app/schemas/auth/refresh_schema.py
+++
@@ -1,5 +0,0 @@
-from app.schemas import BaseSchema
-from app.schemas.custom_types import JwtStr
-
-class RefreshRequest(BaseSchema):
-    refresh_token: JwtStr
--- app/views/auth/unlock_view.py
+++
@@ -1,7 +0,0 @@
-from sanic import Request
-from sanic.views import HTTPMethodView
-
-# ----- Mở khoá tài khoản -----
-class UnlockView(HTTPMethodView):
-    async def get(self, request: Request):
-        pass
